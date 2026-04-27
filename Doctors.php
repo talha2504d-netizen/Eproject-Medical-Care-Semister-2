cd7002c3c9b8093aaa12c5ab8cfa46721307d15b
talha2504d-netizen <talha2504d@aptechsite.net>
2026-04-27 17:01:55 +0500

    Add files via upload

--- Doctors.php
+++ Doctors.php
@@ -4,11 +4,103 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
 </head>
+
 <body>
-    <?php
+    
+<?php
         include("navbar.php");
     ?>
-    <h1>Doctors Page</h1>
+
+    <h1 class="text-center" style="margin: 100px 0px;" >Find Doctors</h1>
+
+    <div class="container">
+
+        <div class="card mb-3 d-flex flex-row align-items-center mx-auto border-bottom border-dark border-start-0 border-end-0 border-top-0 rounded-0" style="max-width: 1050px;">
+
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTwRCFnLvNz4hiLj7Nwt64EPJGc7k4la-HSBQ&s" class="rounded-circle align-self-start" style="width: 150px; height: 150px; object-fit: cover; " alt="...">
+            
+            <div class="card-body ">
+                <h5 class="card-title">Dr Ali</h5>
+                <p class="card-text">Dermatologist, Cosmetologist</p>
+                <p class="card-info">MBBS, FCPS (Dermatology), D-DERM </p>
+               
+                <div class="d-flex flex-row gap-3"> 
+                    <div>
+                        <h6>10 Years</h6>
+                        <p>Experience</p>
+                    </div>
+                
+                    <div>  
+                        <h6> ⭐⭐5.7</h6>
+                        <p>1,127 Reviews</p>
+                    </div>
+                </div>
+            </div>
+
+            <div> 
+                <a href="#" class="btn btn-primary">Video Consultation</a>
+                <br><br>
+                <a href="#" class="btn btn-primary">Book Appointment</a>
+            </div>
+        </div>
+
+    </div>
+
+    <div class="container">
+        <div class="card" style="width: 18rem;">
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTwRCFnLvNz4hiLj7Nwt64EPJGc7k4la-HSBQ&s" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">Dr Name</h5>
+                <p class="card-text">MBBS | FCPS</p>
+                <h6>5 Years</h6>
+                <a href="#" class="btn btn-primary">Book Appointment</a>
+            </div>
+        </div>
+
+    </div>
+
+    <div class="container">
+        <div class="card" style="width: 18rem;">
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTwRCFnLvNz4hiLj7Nwt64EPJGc7k4la-HSBQ&s" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">Dr Name</h5>
+                <p class="card-text">MBBS | FCPS</p>
+                <h6>5 Years</h6>
+                <a href="#" class="btn btn-primary">Book Appointment</a>
+            </div>
+        </div>
+
+    </div>
+
+    <div class="container">
+        <div class="card" style="width: 18rem;">
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTwRCFnLvNz4hiLj7Nwt64EPJGc7k4la-HSBQ&s" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">Dr Name</h5>
+                <p class="card-text">MBBS | FCPS</p>
+                <h6>5 Years</h6>
+                <a href="#" class="btn btn-primary">Book Appointment</a>
+            </div>
+        </div>
+
+    </div>
+
+    <div class="container">
+        <div class="card" style="width: 18rem;">
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTwRCFnLvNz4hiLj7Nwt64EPJGc7k4la-HSBQ&s" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">Dr Name</h5>
+                <p class="card-text">MBBS | FCPS</p>
+                <h6>5 Years</h6>
+                <a href="#" class="btn btn-primary">Book Appointment</a>
+            </div>
+        </div>
+
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
 </body>
 </html>
--- SignUp.php
+++ SignUp.php
@@ -6,39 +6,36 @@
     <title>Document</title>
 </head>
 <body>
-    <h2>Log in to an existing Account</h2>
-    <form action="login.php" METHOD="post">
+    <h2>Create a new Account</h2>
+    <form action="signup.php" METHOD="post">
         Username : <input type="text" name="username" id="username">
         <br>
         Password: <input type="password" name="password" id="password">
         <br>
-        <input type="submit" value="Login" name="login">
-        <a href="signup.php">Or Sign Up for the first time.</a>
+        <input type="submit" value="Submit" name="submit">
+        <a href="login.php">Or Login instead.</a>
     </form>
 </body>
 </html>
 
 <?php
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        if (isset($_POST["login"])) {
+        if (isset($_POST["submit"])) {
             $username = $_POST["username"];
             $password = $_POST["password"];
         }
 
 
         // coonect to database
-        $conn = mysqli_connect('localhost', 'root', '', 'users');
+        $conn = mysqli_connect('localhost', 'root', '', 'care');
 
-        // make get query
-        $sql = "SELECT * FROM users WHERE user_name = '$username' and password = '$password';";
+        $sql = "INSERT INTO patients(username, password) VALUE ('$username', '$password');";
 
-        // run the query
         $conn = mysqli_query($conn, $sql);
 
-        // get result as a n array
-        $result = mysqli_fetch_assoc($conn);
-        echo $result["username"];
-        echo $result["password"];
+        $sql = "SELECT * FROM patients;";
+        // $result = mysqli_fetch_assoc($conn);
+        // echo $result;
         // if (mysqli_num_rows($result) > 0) {
 
 
