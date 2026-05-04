4363ddacab1f684195045cf1e2911290065134aa
mohsinkhan407 <mohsinkhan@aptechsite.net>
2026-05-04 15:25:05 +0500

    add basic structure for index page

--- Index.php
+++ Index.php
@@ -9,5 +9,35 @@
     <?php
         include("navbar.php");
     ?>
+
+    <!-- main content -->
+    <div class="container">
+        <div class="row">
+            <h3>Services</h3>
+            <!-- content here -->
+
+        </div>
+        <div class="row">
+             Categories
+            <!-- content here -->
+        </div>
+        <div class="row">
+            <h3>Conditions</h3>
+            <!-- content here -->
+        </div>
+        <div class="row">
+            <h3>Blog Articles</h3>
+            <!-- content here -->
+        </div>
+        <div class="row">
+            <h3>Reviews</h3>
+            <!-- content here -->
+        </div>
+    </div>
+
+    <!-- Footer Goes here -->
+    <?php
+        include("navbar.php");
+    ?>
 </body>
 </html>
