d71b8268f3882de18a346a69c069462ad3375018
tayyab-imran-616 <tayyab2509d@aptechsite.net>
2026-05-12 11:46:26 +0500

    fix header/footer missing error

--- HealthBlog.php
+++ HealthBlog.php
@@ -1,9 +1,7 @@
 <?php
-include("../navbar.php");
+include("navbar.php");
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -380,8 +378,7 @@ include("../navbar.php");
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 
 </body>
-</html>
 
 <?php
-include("../Footer.php");
+include("Footer.php");
 ?>
